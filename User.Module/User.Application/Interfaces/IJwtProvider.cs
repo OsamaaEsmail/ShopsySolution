3f0a230f82582c26317e628faf5ad4a0512dcc61
Dev Osama Esmail <devosama291@gmail.com>
2026-03-16 21:27:04 +0200

    feat(User): add JWT authentication provider

---
+++ User.Module/User.Application/Interfaces/IJwtProvider.cs
@@ -0,0 +1,14 @@
+﻿
+
+
+
+using User.Domain.Entities;
+
+namespace User.Application.Interfaces;
+
+public interface IJwtProvider
+{
+    (string token, int expiresIn) GenerateToken(ApplicationUser user, IEnumerable<string> roles, IEnumerable<string> permissions);
+
+    string? ValidateToken(string token);
+}
