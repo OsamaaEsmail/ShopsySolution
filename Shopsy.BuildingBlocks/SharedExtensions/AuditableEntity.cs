03006d7b20b95f09a7445b9d1cfea78b56d023fe
Dev Osama Esmail <devosama291@gmail.com>
2026-03-17 22:26:51 +0200

    feat: add AuditableEntity and SaveChangesAsync auditing
    - Shopsy.BuildingBlocks: AuditableEntity abstract class (CreatedById, CreatedOn, UpdatedById, UpdatedOn)
    - UserDbContext: override SaveChangesAsync to auto-fill audit fields on Add/Modify
    - InitialFixedUser migration

---
+++ Shopsy.BuildingBlocks/SharedExtensions/AuditableEntity.cs
@@ -0,0 +1,13 @@
+﻿
+
+
+
+namespace Shopsy.BuildingBlocks.SharedExtensions;
+
+public abstract class AuditableEntity
+{
+    public string CreatedById { get; set; } = string.Empty;
+    public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+    public string? UpdatedById { get; set; }
+    public DateTime? UpdatedOn { get; set; }
+}
--- User.Module/User.Infrastructure/Persistence/UserDbContext.cs
+++ User.Module/User.Infrastructure/Persistence/UserDbContext.cs
@@ -1,30 +1,30 @@
-﻿
-
-
-
-
-
-
-
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Shopsy.BuildingBlocks.SharedExtensions;
 using System.Reflection;
+using System.Security.Claims;
 using User.Domain.Entities;
 
 namespace User.Infrastructure.Persistence;
 
-public class UserDbContext(DbContextOptions<UserDbContext> options)
-    : IdentityDbContext<ApplicationUser, ApplicationRole, string>(options)
+public class UserDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, string>
 {
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public UserDbContext(DbContextOptions<UserDbContext> options, IHttpContextAccessor httpContextAccessor)
+        : base(options)
+    {
+        _httpContextAccessor = httpContextAccessor;
+    }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
 
         modelBuilder.HasDefaultSchema("users");
-
         modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
 
-        // Disable cascade delete
         var cascadeFKs = modelBuilder.Model
             .GetEntityTypes()
             .SelectMany(t => t.GetForeignKeys())
@@ -32,5 +32,32 @@ public class UserDbContext(DbContextOptions<UserDbContext> options)
 
         foreach (var fk in cascadeFKs)
             fk.DeleteBehavior = DeleteBehavior.Restrict;
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        var entries = ChangeTracker.Entries<AuditableEntity>();
+
+        var currentUserId = _httpContextAccessor.HttpContext?.User
+            .FindFirstValue(ClaimTypes.NameIdentifier);
+
+        if (string.IsNullOrEmpty(currentUserId))
+            currentUserId = Domain.Consts.DefaultUsers.Admin.Id;
+
+        foreach (var entityEntry in entries)
+        {
+            if (entityEntry.State == EntityState.Added)
+            {
+                entityEntry.Property(x => x.CreatedById).CurrentValue = currentUserId;
+                entityEntry.Property(x => x.CreatedOn).CurrentValue = DateTime.UtcNow;
+            }
+            else if (entityEntry.State == EntityState.Modified)
+            {
+                entityEntry.Property(x => x.UpdatedById).CurrentValue = currentUserId;
+                entityEntry.Property(x => x.UpdatedOn).CurrentValue = DateTime.UtcNow;
+            }
+        }
+
+        return base.SaveChangesAsync(cancellationToken);
     }
 }
